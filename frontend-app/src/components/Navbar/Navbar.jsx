08d5725d14fcd4489e91322c427fc3d3bcc1ae0b
Kishore <kishoreharry2001@gmail.com>
2024-05-02 13:27:06 +0530

    home

--- frontend-app/src/components/Navbar/Navbar.jsx
+++ frontend-app/src/components/Navbar/Navbar.jsx
@@ -1,31 +1,34 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
   return (
-    <div className='bg-transparent mx-auto flex items-center justify-between px-6 py-2 lg:px-12 lg:py-4 drop-shadow'>
-      <h2 className='lg:text-2xl text-xl font-medium text-white py-2'>SmartAudit</h2>    
+    <div className="bg-black mx-auto flex items-center justify-between px-6 py-2 lg:px-8 lg:py-3 drop-shadow sticky top-0 z-50">
+      <div className="lg:text-2xl text-xl font-medium text-white py-2">
+        SmartAudit
+      </div>
 
-    <div className='flex items-center space-x-16'>
-        <div className='hidden lg:flex items-center gap-8'>
-            <Link className='text-xl font-medium text-white py-2 hover:border-b-4 hover:border-blue-500 '>Home</Link>    
-            <Link className='text-xl font-medium text-white py-2 hover:border-b-4 hover:border-blue-500 '>Services</Link>    
-            <Link className='text-xl  font-medium text-white py-2 hover:border-b-4 hover:border-blue-500 '>Audits</Link>    
-            <Link className='text-xl  font-medium text-white py-2 hover:border-b-4 hover:border-blue-500 '>Solutions</Link>    
-            <Link className='text-xl  font-medium text-white py-2 hover:border-b-4 hover:border-blue-500 '>Contact Us</Link>    
-        
+      <div className="flex items-center py-2 space-x-16">
+        <div className="hidden lg:flex items-center gap-8">
+          <Link className="text-md font-poppins font-medium text-white py-2  hover:text-blue-500 ">
+            Services
+          </Link>
+          <Link className="text-md  font-medium text-white py-2 hover:text-blue-500 ">
+            Audits
+          </Link>
+
+          <Link className="text-md  font-medium text-white py-2  hover:text-blue-500 ">
+            Contact Us
+          </Link>
         </div>
-
-        <button className='text-white lg:text-xl text-sm bg-blue-500 px-3 rounded-lg py-2'>
-            Request Quote
+      </div>
+      <div>
+        <button className="text-white  text-md bg-blue-800 px-4  rounded-full py-2">
+          Request Quote
         </button>
-
+      </div>
     </div>
+  );
+};
 
-        
-    </div>
-  )
-}
-
-export default Navbar
+export default Navbar;
